4a5d3e2981fbd2930f717e1efdc59f53fa02fad8
Harsheetsharma <harshitsharma8012@gmail.com>
2025-07-29 17:52:00 +0530

    notification added

--- apps/user-app/components/Recentp2pTxn.tsx
+++ apps/user-app/components/Recentp2pTxn.tsx
@@ -79,32 +79,6 @@ export const Recentp2pTxn = ({
 
   return (
     <div>
-      {/* <Card title="Your Recent Transactions">
-        <div className="pt-2">
-          {transactions.map((t) => (
-            <div className="flex justify-between border-b mb-1">
-              <div>
-                <div className="text-sm">
-                  {t.direction === "sent"
-                    ? `INR ${t.amount / 100} Sent to ${t.counterParty}`
-                    : `INR ${t.amount / 100} Received from ${t.counterParty}`}
-                </div>
-                <div className="text-slate-600 text-xs">
-                  {t.timeStamp.toDateString()}
-                </div>
-              </div>
-              <div
-                className={`flex flex-col justify-center font-semibold ${
-                  t.direction === "sent" ? "text-red-600" : "text-green-600"
-                }`}
-              >
-                {t.direction === "sent" ? "-" : "+"} Rs {t.amount / 100}
-              </div>
-            </div>
-          ))}
-        </div>
-      </Card> */}
-
       <Cards>
         <CardHeader>
           <div className="flex justify-between items-center">
