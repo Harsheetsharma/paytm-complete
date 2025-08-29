228d89da6a6274b8839840c8870f1ac224a4b7c2
Harsheetsharma <harshitsharma8012@gmail.com>
2025-08-29 18:49:12 +0530

    fix

--- apps/user-app/components/stripePayment.tsx
+++ apps/user-app/components/stripePayment.tsx
@@ -36,7 +36,7 @@ export function StripePayment({ amount }: { amount: number }) {
       <button
         onClick={handlepayment}
         disabled={loading}
-        className="text-white bg-blue-600 border hover:bg-blue-700  focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs sm:text-sm px-3 py-1.5 sm:px-5 sm:py-2.5 me-2 mb-2 transition-colors duration-200 ease-in-out"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-500 active:scale-[0.98] disabled:opacity-50 disabled:cursor-not-allowed"
       >
         {loading ? "Redirecting..." : "Add via stripe"}
       </button>
