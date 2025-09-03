74f447e6f192c4e021c59e7dfc6ca500af2fd711
Harsheetsharma <harshitsharma8012@gmail.com>
2025-09-03 18:31:46 +0530

    fixed 2.3

--- apps/user-app/app/(dashboard)/p2p/page.tsx
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -8,7 +8,7 @@ import { redirect } from "next/navigation";
 
 export default async function page() {
   const session = await getServerSession(authOptions);
-  if (session?.user.id) {
+  if (!session?.user.id) {
     redirect("/");
   }
 
